d189b3d792c424b6136a827584f964153dfefeb2
Sam Magura <srmagura@gmail.com>
2023-03-15 16:46:57 -0700

    text input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
+import useInterval from "use-interval";
+
+import { TextInput } from "@mantine/core";
+
 function App() {
-  return <div>test</div>;
+  const [, setCounter] = useState(0);
+
+  useInterval(() => setCounter((c) => c + 1), 250);
+
+  return (
+    <div>
+      <h3>TextInput</h3>
+      <TextInput label="A" />
+      <TextInput label="B" />
+      <TextInput label="C" />
+      <TextInput label="D" />
+      <TextInput label="E" />
+    </div>
+  );
 }
 
 export default App;
